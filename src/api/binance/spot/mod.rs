9ca529416e15d28008232898e3ccd16e5e3a8f57
Sricor <u@ioaths.com>
2024-05-21 22:01:42 +0800

    chore: fmt

--- src/api/binance/spot/mod.rs
+++ src/api/binance/spot/mod.rs
@@ -1,3 +1,4 @@
+pub mod account;
 pub mod order;
 
 pub mod price {
@@ -35,69 +36,30 @@ pub mod price {
     }
 }
 
-pub mod asset {
-    pub mod post_asset {
-        pub const PATH: &str = "/binance/spot/asset";
+pub mod normal {
+    pub mod get_normal {
+        pub const PATH: &str = "/binance/spot/normal";
 
-        use binance::types::{Asset, UserAsset};
+        use binance::types::{ExchangeInfo, Symbol};
         use serde::{Deserialize, Serialize};
 
-        use crate::api::http::request::Json;
+        use crate::api::http::request::Query;
         use crate::api::http::response::{Response, ResponseResult};
         use crate::api::http::trip::Trip;
-        use crate::services::binance::client_with_sign;
+
+        use crate::services::binance::client;
 
         #[derive(Debug, Serialize, Deserialize)]
-        pub struct Payload {
-            api_key: String,
-            secret_key: String,
-            asset: Option<Asset>,
-        }
-
-        pub type Reply = Vec<UserAsset>;
-
-        #[tracing::instrument(skip(_c))]
-        pub async fn handler(_c: Trip, Json(p): Json<Payload>) -> ResponseResult<Reply> {
-            let client = client_with_sign(p.api_key, p.secret_key)?;
-
-            let asset = match &p.asset {
-                Some(v) => Some(v),
-                None => None,
-            };
-
-            let result = client.user_asset(asset, Some(false), None).await?;
-
-            Ok(Response::ok(result))
-        }
-    }
-}
-
-pub mod commission {
-    pub mod post_commission {
-        pub const PATH: &str = "/binance/spot/commission";
-
-        use binance::types::{SpotCommission, Symbol};
-        use serde::{Deserialize, Serialize};
-
-        use crate::api::http::request::Json;
-        use crate::api::http::response::{Response, ResponseResult};
-        use crate::api::http::trip::Trip;
-        use crate::services::binance::client_with_sign;
-
-        #[derive(Debug, Serialize, Deserialize)]
-        pub struct Payload {
-            api_key: String,
-            secret_key: String,
+        pub struct Params {
             symbol: Symbol,
         }
 
-        pub type Reply = SpotCommission;
+        type Reply = ExchangeInfo;
 
         #[tracing::instrument(skip(_c))]
-        pub async fn handler(_c: Trip, Json(p): Json<Payload>) -> ResponseResult<Reply> {
-            let client = client_with_sign(p.api_key, p.secret_key)?;
-
-            let result = client.spot_commission(&p.symbol).await?;
+        pub async fn handler(_c: Trip, Query(p): Query<Params>) -> ResponseResult<Reply> {
+            let client = client()?;
+            let result = client.exchange_info(&p.symbol).await?;
 
             Ok(Response::ok(result))
         }
